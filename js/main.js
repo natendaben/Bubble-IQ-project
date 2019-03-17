63c2ad15ad065a4c06b179aa8396ca2af7aac731
Nate Bennett <nathanael.bennett@colorado.edu>
2019-03-16 21:37:45 -0600

    first commit and document setup

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var list = document.getElementById("list");
+var newEntry = document.createElement("li");
+newEntry.appendChild(document.createTextNode("new list item"));
+list.appendChild(newEntry);
